a3855326e4f0ca00994358b87644fbe1ae561ba9
Kaedh <leonardomds2013@gmail.com>
2021-03-09 21:15:15 -0300

    :sparkles: setting up themes files

--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     grid-area: CT;
-    background-color: orange;
+    background-color: ${ props => props.theme.colors.primary };
 `;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'Light',
+
+    colors: {
+        primary: '#DCDCDC',
+        secondary: '#FFFFFF',
+        tertiary: '#F5F5F5',
+
+        white: '#000',
+        black: '#FFF',
+        gray: '#BFBFBF',
+
+        sucess: '#03BB85',
+        info: '#3B5998',
+        warnig: '#FF6961',
+    }
+};
