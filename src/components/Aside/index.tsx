813b84f5b8187b5963b2104fd3e5e89cc42488a3
Kaedh <leonardomds2013@gmail.com>
2021-03-16 17:59:44 -0300

    :sparkles: implemented aside menu

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-import { Container } from './style';
+import logoImg from '../../assets/logo.svg';
+
+import  { 
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md';
+
+import { Container,
+         Header,
+         LogoImg,
+         Title,
+         MenuContainer,
+         MenuItemLink   } from './style';
 
 const Aside :React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogoImg src={ logoImg } alt="logo" />
+                <Title>PIG</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href='#'> <MdDashboard />   Dashboard</MenuItemLink>
+                <MenuItemLink href='#'> <MdArrowUpward /> Entradas</MenuItemLink>
+                <MenuItemLink href='#'> <MdArrowDownward /> Saídas</MenuItemLink>
+                <MenuItemLink href='#'> <MdExitToApp /> Sair</MenuItemLink>
+            </MenuContainer>
         </Container>
     )
 };
