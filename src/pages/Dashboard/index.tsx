5c0c7be841216c7a82f969c822064fb80e4cb548
Kaedh <leonardomds2013@gmail.com>
2021-03-17 16:00:22 -0300

    :sparkles: creating select component and making content header dynamic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ContentHeader from '../../components/contentHeader';
+import SelectInput from '../../components/SelectInput';
 
 import { Container } from './style';
 
 const Dashboard :React.FC = () => {
+    const options = [{
+        value: 'Leonardo',
+        label: 'Leonardo'
+    }];
+
     return (
         <Container>
-            <ContentHeader />
+            <ContentHeader title="Dashboard" lineColor="#fff">
+                <SelectInput options={options}/>
+            </ContentHeader>
         </Container>
     )
 };
