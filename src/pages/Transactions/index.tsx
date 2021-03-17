ab8c12eaebf707d5e7674b38342013058eda1004
Kaedh <leonardomds2013@gmail.com>
2021-03-17 17:32:05 -0300

    :sparkles: creating legends

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,19 +3,35 @@ import ContentHeader from '../../components/contentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container, Content } from './style';
+import { Container, Content, Filters } from './style';
 
 const Transactions :React.FC = () => {
-    const options = [{
-        value: 'Leonardo',
-        label: 'Leonardo'
-    }];
+    const months = [
+        {value: 3, label: 'Março'},
+        {value: 1, label: 'Janeiro'},
+        {value: 2, label: 'Fevereiro'},
+        {value: 4, label: 'Abril'},
+    
+    ];
+
+    const years = [
+        {value: 2021, label: 2021},
+        {value: 2019, label: 2019},
+        {value: 2020, label: 2020},
+    ];
+
     
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#E44C4E">
-                <SelectInput options={options}/>
+                <SelectInput options={months}/>
+                <SelectInput options={years}/>
             </ContentHeader>
+
+            <Filters>
+                <button type="button" className="tag-filter tag-filter-recurrent"> Recorrentes </button>
+                <button type="button" className="tag-filter tag-filter-eventual"> Eventuais </button>
+            </Filters>
 
             <Content>
                 <HistoryFinanceCard 
